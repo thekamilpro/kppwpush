31f942abbaab55e3138d9bb669e018f511597e49
Kamil Procyszyn <kprocyszyn@gmail.com>
2022-11-01 20:19:23 +0000

    Implement module building

---
+++ build-scripts/Start-ModuleBuild.ps1
@@ -0,0 +1,32 @@
+param (
+    $Version = "1.0.0",
+
+    $Name = "KpPwpush"
+)
+
+#Requires -Module "ModuleBuilder"
+
+$params = @{
+    SourcePath = "$PSScriptRoot/../src/KpPwpush.psd1" 
+    UnversionedOutputDirectory = $true
+    Version = $Version 
+    Passthru = $true
+}
+
+Write-Host "Building module $Name $Version"
+$result = Build-Module @params
+
+try 
+{
+    Write-Host "Loading module"
+    Import-Module -Name $result.Path -ErrorAction stop
+}
+catch
+{
+    Throw "Failed to load module $_"
+}
+finally
+{
+    Write-Host "Unloading module"
+    Remove-Module -Name $result.Name -ErrorAction SilentlyContinue
+}

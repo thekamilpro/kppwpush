b803d197262d167338a68a212799942bed668076
Kamil Pro <kprocyszyn@gmail.com>
2024-05-06 17:52:01 +0100

    Add URL active

---
+++ src/public/URL/Get-PwpushUrlActive.ps1
@@ -0,0 +1,18 @@
+﻿function Get-PwpushUrlActive
+{
+    <#
+    .SYNOPSIS
+        Returns the list of URL pushes that you previously pushed which are still active.
+    #>
+    [cmdletbinding()]
+    param (
+    )
+
+    $endpoint = "r/active.json"
+
+    $params = @{
+        Endpoint = $endpoint
+        Method   = "Get"
+    }
+    Invoke-PwpushRequest @params
+}
